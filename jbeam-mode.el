0c5fe9737575d046b79254e959c6b76489ad5ce7
debbie <148627186+webdevred@users.noreply.github.com>
2025-10-07 22:12:57 +0200

    Added formal license header

--- jbeam-mode.el
+++ jbeam-mode.el
@@ -8,6 +8,20 @@
 ;;
 ;; This file is NOT part of GNU Emacs.
 ;;
+;; Copyright 2025 August Johansson
+;;
+;;   Licensed under the Apache License, Version 2.0 (the "License");
+;;   you may not use this file except in compliance with the License.
+;;   You may obtain a copy of the License at
+;;
+;;       http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;   Unless required by applicable law or agreed to in writing, software
+;;   distributed under the License is distributed on an "AS IS" BASIS,
+;;   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;   See the License for the specific language governing permissions and
+;;   limitations under the License.
+;;
 ;;; Commentary:
 ;;
 ;; This package provides a major mode `jbeam-mode` for editing
@@ -42,8 +56,7 @@
 
     ("[{}\\[\\]]" . font-lock-builtin-face)
 
-    ("//.*$" . font-lock-comment-face)
-    )
+    ("//.*$" . font-lock-comment-face))
   "Highlighting for `jbeam-mode'.")
 
 ;;;###autoload
